820a24b0b263acd151384334fa1934c7d1c5eb7f
xkstndua15 <xkstndua15@gmail.com>
2022-07-11 21:45:29 +0900

    header 디자인 변경

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,30 +1,43 @@
 import { NavLink, Link } from 'react-router-dom';
 
-function Header() {
+function Header(props) {
+	const color = { color: '#fff' };
 	return (
-		<header>
+		<header className={props.type}>
 			<div className='inner'>
 				<h1>
 					<Link to='/'>LOGO</Link>
 				</h1>
 				<ul id='gnb'>
 					<li>
-						<NavLink to='/department'>Department</NavLink>
+						<NavLink to='/department' activeStyle={color}>
+							Department
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/gallery'>Gallery</NavLink>
+						<NavLink to='/gallery' activeStyle={color}>
+							Gallery
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/youtube'>Youtube</NavLink>
+						<NavLink to='/youtube' activeStyle={color}>
+							Youtube
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/community'>Community</NavLink>
+						<NavLink to='/community' activeStyle={color}>
+							Community
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/location'>Location</NavLink>
+						<NavLink to='/location' activeStyle={color}>
+							Location
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/membership'>Membership</NavLink>
+						<NavLink to='/membership' activeStyle={color}>
+							Membership
+						</NavLink>
 					</li>
 				</ul>
 			</div>
