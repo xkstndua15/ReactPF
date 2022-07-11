33741565178e826b6a79587f12de461d6c155e78
xkstndua15 <xkstndua15@gmail.com>
2022-07-11 21:45:37 +0900

    department 디자인 변경

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -14,56 +14,62 @@ function Department() {
 	}, []);
 
 	return (
-		<Layout name={'Department'} desc={desc}>
-			<div className='main'>
-				<div className='visual'>
-					<img
-						src={`${path}/img/sub/department/visual.png`}
-						alt='조직도 사람들이 모여있는 이미지'
-					/>
+		<>
+			<Layout name={'Department'} desc={desc}>
+				<div className='main'>
+					<div className='visual'>
+						<img
+							src={`${path}/img/sub/department/visual.png`}
+							alt='조직도 사람들이 모여있는 이미지'
+						/>
+					</div>
+					<ul>
+						<li>
+							<h2>1. Strategy</h2>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Recusandae, consequuntur?
+							</p>
+						</li>
+						<li>
+							<h2>2. Synchronize</h2>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+								vitae!
+							</p>
+						</li>
+						<li>
+							<h2>3. Control</h2>
+							<p>
+								Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos,
+								molestias!
+							</p>
+						</li>
+					</ul>
 				</div>
-				<ul>
-					<li>
-						<h2>1. Strategy</h2>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit.
-							Recusandae, consequuntur?
-						</p>
-					</li>
-					<li>
-						<h2>2. Synchronize</h2>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-							vitae!
-						</p>
-					</li>
-					<li>
-						<h2>3. Control</h2>
-						<p>
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos,
-							molestias!
-						</p>
-					</li>
-				</ul>
+			</Layout>
+			<div className='background'>
+				<div className='wrap'>
+					<h3>OUR TEAM</h3>
+					{Members.map((member, idx) => {
+						return (
+							<article key={idx}>
+								<div className='inner'>
+									<div className='pic'>
+										<img
+											src={`${path}/img/sub/department/${member.pic}`}
+											alt={`${path}/img/sub/department/${member.name}`}
+										/>
+									</div>
+									<h4>{member.name}</h4>
+									<p>{member.position}</p>
+								</div>
+							</article>
+						);
+					})}
+				</div>
 			</div>
-			<h3>OUR TEAM</h3>
-			{Members.map((member, idx) => {
-				return (
-					<article key={idx}>
-						<div className='inner'>
-							<div className='pic'>
-								<img
-									src={`${path}/img/sub/department/${member.pic}`}
-									alt={`${path}/img/sub/department/${member.name}`}
-								/>
-							</div>
-							<h4>{member.name}</h4>
-							<p>{member.position}</p>
-						</div>
-					</article>
-				);
-			})}
-		</Layout>
+		</>
 	);
 }
 
