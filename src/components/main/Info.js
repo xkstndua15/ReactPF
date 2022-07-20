2efe722a16e226c14aacd4e939e21c252a0a4fbd
xkstndua15 <xkdtndua15@gmail.com>
2022-07-20 16:21:59 +0900

    메인페이지 레이아웃 작업 완료 (News, Info)

--- src/components/main/Info.js
+++ src/components/main/Info.js
@@ -1,5 +1,41 @@
 function Info() {
-	return <div>Info</div>;
+	return (
+		<section id='info'>
+			<div className='typoInner'>
+				<h2>
+					Are you ready to
+					<br />
+					upgrade your
+					<br />
+					product with us?
+				</h2>
+				<p>
+					Maybe your question is have been
+					<br />
+					answered, check this out.
+				</p>
+			</div>
+			<div className='btnInner'>
+				<div className='wrap'>
+					<div className='dropItem'>
+						<ul>
+							<li>What is LOGO?</li>
+							<li>How to buy our services?</li>
+							<li>Making custom services</li>
+						</ul>
+					</div>
+					<div className='textItem'>
+						<h3>
+							We're always answered your question,
+							<br />
+							and we give you free quote!
+						</h3>
+						<button>Let's Talk</button>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default Info;
--- src/components/main/News.js
+++ src/components/main/News.js
@@ -1,5 +1,36 @@
 function News() {
-	return <div>News</div>;
+	return (
+		<section id='news'>
+			<div className='inner'>
+				<video src=''></video>
+				<div className='wrap'>
+					<ul>
+						<li>
+							<h3>1. Strategy</h3>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Recusandae, consequuntur?
+							</p>
+						</li>
+						<li>
+							<h3>2. Synchronize</h3>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+								vitae!
+							</p>
+						</li>
+						<li>
+							<h3>3. Control</h3>
+							<p>
+								Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos,
+								molestias!
+							</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default News;
